22de485cd69ec2651527570d28b40e0d1ea79a7f
Marcus <marcus.isaksson@elev.ga.ntig.se>
2020-11-30 08:36:14 +0100

    start

---
+++ GFX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Raylib_cs;
+
+namespace GFX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Raylib.InitWindow(800, 600, "bildtest");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground(Color.BEIGE);
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
