904db5924c0867f9fd8c6e299540febd568f8246
m-isaksson84 <marcus.isaksson@elev.ga.ntig.se>
2020-12-26 18:09:38 +0100

    Påbörjade projektet, lade till movement och meny

--- GFX/Program.cs
+++ GFX/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Raylib_cs;
+using System.Numerics;
 
 namespace GFX
 {
@@ -7,17 +8,86 @@ namespace GFX
     {
         static void Main(string[] args)
         {
-
-            Raylib.InitWindow(800, 600, "bildtest");
-
+            
+            Raylib.InitWindow(2000 , 1200 , "bildtest");
+            Texture2D SD = Raylib.LoadTexture("character.png");
+            float posX = 10;
+            float posY = 10;
+            float Speed = 0.3f;
+            bool gameStarted;
+            gameStarted = false;
+            Vector2 mousePos = Raylib.GetMousePosition();
+        
             while (!Raylib.WindowShouldClose())
             {
-                Raylib.BeginDrawing();
+                float width = 2000;
+                float height = 1200;
+                float xMovement = 0;
+                float yMovement = 0;
 
-                Raylib.ClearBackground(Color.BEIGE);
+                if (gameStarted == false) {
+                    mousePos = Raylib.GetMousePosition();
+                    Rectangle startGame = new Rectangle(620, 400, 620, 90);
+                    bool areOverlapping = Raylib.CheckCollisionPointRec(mousePos, startGame); 
+                    bool leftMousePressed = Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON);
 
-                Raylib.EndDrawing();
-            }
+                    Raylib.ClearBackground(Color.RED);     
+                    Raylib.DrawText("Spel!", 800, 200, 100, Color.ORANGE);
+                    Raylib.DrawText("Starta Spelet", 620, 400, 90, Color.ORANGE);
+
+                    Raylib.EndDrawing();  
+
+                    if (areOverlapping == true)
+                    {
+                    
+                        if (leftMousePressed == true)
+                        {
+                            gameStarted = true;
+                        }
+                    }   
+
+                    if (Raylib.IsKeyPressed(KeyboardKey.KEY_X))
+                    {
+                        gameStarted = true;
+                    }
+                }
+
+                if (gameStarted == true) {
+                    
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && posY < height - 150)
+                    {
+                        yMovement = Speed;
+                    }
+
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && posY > 0)
+                    {
+                        yMovement = -Speed;
+                    }
+
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && posX < width - 112.5)
+                    {
+                        xMovement = Speed;
+                    }
+
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_A) && posX > 0)
+                    {
+                        xMovement = -Speed;
+                    }
+
+                    posX += xMovement;
+                    
+                    posY += yMovement;
+                    Raylib.BeginDrawing();
+                    Raylib.SetExitKey(KeyboardKey.KEY_Q);
+
+                    Raylib.DrawTextureEx(SD, new Vector2(posX, posY), 0f, 0.25f, Color.WHITE);
+
+                // bool areOverlapping = Raylib.CheckCollisionPointRec(mousePos, SD);
+                    
+                    Raylib.ClearBackground(Color.BEIGE);
+                    Raylib.EndDrawing();
+                }
+           }
         }
     }
 }
