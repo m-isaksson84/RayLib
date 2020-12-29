45758bf552600f637a185fbd8acbc6b7e2bf7d4f
m-isaksson84 <marcus.isaksson@elev.ga.ntig.se>
2020-12-29 17:19:23 +0100

    Började skapa en primitiv kollisionslåda

--- GFX/Program.cs
+++ GFX/Program.cs
@@ -13,7 +13,10 @@ namespace GFX
             Texture2D SD = Raylib.LoadTexture("character.png");
             float posX = 10;
             float posY = 10;
-            float Speed = 0.3f;
+            float xSpeed = 0.3f;
+            float ySpeed = 0.3f;
+            float xSpeedN = 0.3f;
+            float ySpeedN = 0.3f;
             bool gameStarted;
             gameStarted = false;
             Vector2 mousePos = Raylib.GetMousePosition();
@@ -24,13 +27,18 @@ namespace GFX
                 float height = 1200;
                 float xMovement = 0;
                 float yMovement = 0;
+                float xMovementN = 0;
+                float yMovementN = 0;
 
                 if (gameStarted == false) {
 
                     // resettar spelarens potition (gör till en metod som resettar allt)
                     posX = 10;
                     posY = 10;
-                    Speed = 0.3f;
+                    xSpeed = 0.3f;
+                    ySpeed = 0.3f;
+                    xSpeedN = 0.3f;
+                    ySpeedN = 0.3f;
 
 
                     mousePos = Raylib.GetMousePosition();
@@ -60,25 +68,26 @@ namespace GFX
                 }
 
                 if (gameStarted == true) {
+
                     
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && posY < height - 150)
                     {
-                        yMovement = Speed;
+                        yMovement = ySpeed;
                     }
 
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && posY > 0)
                     {
-                        yMovement = -Speed;
+                        yMovementN = -ySpeedN;
                     }
 
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && posX < width - 112.5)
                     {
-                        xMovement = Speed;
+                        xMovement = xSpeed;
                     }
 
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_A) && posX > 0)
                     {
-                        xMovement = -Speed;
+                        xMovementN = -xSpeedN;
                     }
 
                     if (Raylib.IsKeyDown(KeyboardKey.KEY_B))
@@ -87,11 +96,53 @@ namespace GFX
                     }
 
                     posX += xMovement;
-                    
                     posY += yMovement;
+
+                    posX += xMovementN;
+                    posY += yMovementN;
+
                     Raylib.BeginDrawing();
                     Raylib.SetExitKey(KeyboardKey.KEY_Q);
 
+
+                    // sätter upp kollosioner för karaktären genom att testa ifall vector2 (characterCollisionBox) kolliderar med obstacle, returnerar true om de överlappar (kolliderar)
+                    Vector2 characterCollisionBox = new Vector2(posX, posY);
+                    Rectangle obstacle = new Rectangle(500, 500, 1000, 400);
+                    bool characterCollide = Raylib.CheckCollisionPointRec(characterCollisionBox, obstacle); 
+
+                    if (characterCollide == true)
+                    {
+
+                            if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                            {
+                                ySpeedN = 0;
+                            }
+
+                            else if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                            {
+                                xSpeed = 0;
+                            }
+
+                            else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                            {
+                                ySpeed = 0;
+                            } 
+
+                            else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                            {
+                                xSpeedN = 0;
+                            }
+                    }
+
+                    else 
+                    {
+                        xSpeed = 0.3f;
+                        ySpeed = 0.3f;
+                        xSpeedN = 0.3f;
+                        ySpeedN = 0.3f;
+                    }
+
+                    Raylib.DrawRectangle(500, 500, 1000, 400, Color.WHITE);
                     Raylib.DrawTextureEx(SD, new Vector2(posX, posY), 0f, 0.25f, Color.WHITE);
 
                 // bool areOverlapping = Raylib.CheckCollisionPointRec(mousePos, SD);
