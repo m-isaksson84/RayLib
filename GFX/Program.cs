4e15f359d1ade61930c67fb8ad896c3d5b92d470
m-isaksson84 <marcus.isaksson@elev.ga.ntig.se>
2021-01-07 21:25:43 +0100

    fixade kollisioner, mer content

--- GFX/Program.cs
+++ GFX/Program.cs
@@ -9,19 +9,31 @@ namespace GFX
         static void Main(string[] args)
         {
             
-            Raylib.InitWindow(2000 , 1200 , "bildtest");
+            Raylib.InitWindow(2000 , 1200 , "Game Thing! :D");
             Texture2D SD = Raylib.LoadTexture("character.png");
             Vector2 mousePos = Raylib.GetMousePosition();
-            float posX = 10;
-            float posY = 10;
-            // position x-previous resp y-previous
+            float posX = 1800;
+            float posY = 80;
+
+            // position x-previous resp y-previous.
             float posXP = posX;
             float posYP = posY;
+
+            // standardhastighet för spelaren, SpeedN = SpeedNegative.
             float xSpeed = 0.3f;
             float ySpeed = 0.3f;
             float xSpeedN = 0.3f;
             float ySpeedN = 0.3f;
             bool gameStarted;
+
+            // Knappar för menyn.
+            Rectangle startGame = new Rectangle(840, 400, 260, 90);
+            Rectangle options = new Rectangle(800, 600, 330, 90);
+
+            // Hitboxes för spelet
+            Rectangle obstacle = new Rectangle(400, 300, 1600, 600);
+            Rectangle obstacle2 = new Rectangle(0, 0, 100, 1200);
+            Rectangle finishLineLevel1 = new Rectangle(1800, 1000, 100, 200);
             
             gameStarted = false;
         
@@ -37,8 +49,8 @@ namespace GFX
                 if (gameStarted == false) {
 
                     // resettar spelarens potition (gör till en metod som resettar allt)
-                    posX = 10;
-                    posY = 10;
+                    posX = 1800;
+                    posY = 80;
                     xSpeed = 0.3f;
                     ySpeed = 0.3f;
                     xSpeedN = 0.3f;
@@ -46,8 +58,8 @@ namespace GFX
 
 
                     mousePos = Raylib.GetMousePosition();
-                    Rectangle startGame = new Rectangle(840, 400, 260, 90);
-                    Rectangle options = new Rectangle(800, 600, 330, 90);
+
+                
                     bool areOverlappingStart = Raylib.CheckCollisionPointRec(mousePos, startGame);
                     bool areOverlappingOptions = Raylib.CheckCollisionPointRec(mousePos, options);
                     bool leftMousePressed = Raylib.IsMouseButtonPressed(MouseButton.MOUSE_LEFT_BUTTON);
@@ -97,7 +109,7 @@ namespace GFX
                         yMovementN = -ySpeedN;
                     }
 
-                    if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && posX < width - 112.5)
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_D) && posX < width - 113)
                     {
                         xMovement = xSpeed;
                     }
@@ -123,13 +135,36 @@ namespace GFX
 
 
                     // sätter upp kollosioner för karaktären genom att testa ifall vector2 (characterCollisionBox) kolliderar med obstacle, returnerar true om de överlappar (kolliderar)
+
                     Vector2 characterCollisionBox = new Vector2(posX, posY);
                     Vector2 characterCollisionBox1 = new Vector2(posX+113, posY+150);
-                    Rectangle obstacle = new Rectangle(500, 500, 1000, 400);
+                    Vector2 characterCollisionBox2 = new Vector2(posX, posY+150); // vektorn går från posX till PosY+150 vilket skapar en diagonal linje
+                    Vector2 characterCollisionBox3 = new Vector2(posX+113, posY); // vektorn går från PosX+113 till posY vilket skapar en diagonal linje
+                    //Vector2 characterCollisionBox4 = new Vector2(posX, posX+113); // vektorn går från posX till PosX+113 vilket skapar en rak linje längst upp
+                    //Vector2 characterCollisionBox4 = new Vector2(posX+113, posY+75); // vektorn går från posX till PosX+113 vilket skapar en rak linje längst upp
+
+
                     bool characterCollide = Raylib.CheckCollisionPointRec(characterCollisionBox, obstacle);
                     bool characterCollide1 = Raylib.CheckCollisionPointRec(characterCollisionBox1, obstacle);
+                    bool characterCollide2 = Raylib.CheckCollisionPointRec(characterCollisionBox2, obstacle);
+                    bool characterCollide3 = Raylib.CheckCollisionPointRec(characterCollisionBox3, obstacle);
 
-                    if (characterCollide || characterCollide1 == true)
+                    bool test = Raylib.CheckCollisionPointRec(characterCollisionBox, obstacle2);
+                    bool test1 = Raylib.CheckCollisionPointRec(characterCollisionBox1, obstacle2);
+                    bool test2 = Raylib.CheckCollisionPointRec(characterCollisionBox2, obstacle2);
+                    bool test3 = Raylib.CheckCollisionPointRec(characterCollisionBox3, obstacle2);
+      
+                    bool finishLevel = Raylib.CheckCollisionPointRec(characterCollisionBox, finishLineLevel1);
+                    bool finishLevel1 = Raylib.CheckCollisionPointRec(characterCollisionBox1, finishLineLevel1);
+
+
+                    if (test || test1 || test2 || test3 == true)
+                    {
+                        posX = posXP;
+                        posY = posYP;
+                    }
+
+                    if (characterCollide || characterCollide1 || characterCollide2 || characterCollide3 == true)
                     {
 /*
                             if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
@@ -178,8 +213,15 @@ namespace GFX
 
                             */
                             
-                            posX = posXP;
-                            posY = posYP;
+                        posX = posXP;
+                        posY = posYP;
+                    }
+
+                    else if (finishLevel || finishLevel1 == true)
+                    {
+                        posX = posXP;
+                        posY = posYP;
+                        gameStarted = false;
                     }
 
                     else 
@@ -190,7 +232,12 @@ namespace GFX
                         ySpeedN = 0.3f;
                     }
 
-                    Raylib.DrawRectangle(500, 500, 1000, 400, Color.WHITE);
+                    Raylib.DrawRectangle(400, 300, 1600, 600, Color.WHITE);
+                    Raylib.DrawRectangle(0, 0, 100, 1200, Color.WHITE);
+                    Raylib.DrawText("Use WASD to move and navigate to the red square :D", 680, 500, 40, Color.BLACK);
+                    Raylib.DrawRectangle(1800, 1000, 100, 100, Color.RED);
+
+                    Raylib.DrawRectangle(1800, 80, 10, 10, Color.RED);
                     Raylib.DrawTextureEx(SD, new Vector2(posX, posY), 0f, 0.25f, Color.WHITE);
 
                     // logga senaste x och y position
